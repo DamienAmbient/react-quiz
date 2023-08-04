0432e5e007acabbb3af1baf8019ee87c68e80a63
Damien <damienambient@gmail.com>
2023-08-04 22:55:54 +0300

    separated components. Question loaded

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import Question from "./Question";
 const initialState = {
     questions: [],
     status: "loading",
+    index: 0,
 };
 
 function reducer(state, action) {
@@ -36,7 +37,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status, index }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
 
     const numQuestions = questions.length;
 
@@ -60,7 +64,9 @@ export default function App() {
                         dispatch={dispatch}
                     ></StartScreen>
                 )}
-                {status === "active" && <Question></Question>}
+                {status === "active" && (
+                    <Question question={questions[index]}></Question>
+                )}
             </Main>
         </div>
     );
